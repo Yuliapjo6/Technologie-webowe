782fd7fa76780d036e2dca8c399523771965092c
Yuliia Pavlishak <juliapjo3@gmail.com>
2026-05-07 11:31:49 +0200

    zad6

---
+++ script1.js
@@ -0,0 +1,8 @@
+function bth_anim{
+    let anim = document.getElementById("anim")
+    anim.style.animation = "none"
+}
+function bth_anim2{
+    let anim2 = document.getElementById("anim2")
+    anim.style
+}
